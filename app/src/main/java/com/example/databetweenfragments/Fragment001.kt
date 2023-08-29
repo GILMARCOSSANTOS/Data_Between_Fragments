4ff78b4e94669f5a29f705399e1bd2d59c6194cb
GilmarcosSantos <gilmarcosxsantos@gmail.com>
2023-08-29 20:22:01 -0300

    Iniciar alteracoes em MainActvt

--- app/src/main/java/com/example/databetweenfragments/Fragment001.kt
+++ app/src/main/java/com/example/databetweenfragments/Fragment001.kt
@@ -5,21 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 
 
 class Fragment001 : Fragment() {
 
     private lateinit var communicator: Communicator
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-
-
-    }
+    private lateinit var textViewShow: MaterialTextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,12 +25,24 @@ class Fragment001 : Fragment() {
 
         communicator = requireActivity() as Communicator
 
-        val enterBtn = rootView.findViewById<Button>(R.id.enter_btn_id)
-        val inputEditText = rootView.findViewById<EditText>(R.id.input_editText_id)
+        val enterBtn = rootView.findViewById<MaterialButton>(R.id.enter_btn_id)
+        val inputEditText = rootView.findViewById<MaterialTextView>(R.id.txtVw_showText_fragment001_id)
         enterBtn.setOnClickListener {
             communicator.passDataCommunicator(inputEditText.text.toString())
         }
 
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        globalVariablesEscope()
+        textViewShow.text = "G. Santos"
+    }
+
+    private fun globalVariablesEscope(): View {
+        textViewShow = view?.findViewById(R.id.txtVw_showText_fragment001_id)!!
+        return view as View
     }
 }
--- app/src/main/java/com/example/databetweenfragments/Fragment002.kt
+++ app/src/main/java/com/example/databetweenfragments/Fragment002.kt
@@ -1,19 +1,19 @@
 package com.example.databetweenfragments
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 
 class Fragment002 : Fragment() {
 
     private var inputText: String = ""
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +21,18 @@ class Fragment002 : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_002, container, false)
 
-       inputText = arguments?.getString("input_txt").toString()
+        inputText = arguments?.getString("input_txt").toString()
 
         val outputTextView = rootView.findViewById<TextView>(R.id.outPut_TextView_id)
         outputTextView.text = inputText
 
         return rootView
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
 }
